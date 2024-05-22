f3bb3318fcf9c14f5c463fc0a3eba7ddeb59f27f
RoGoLo-05 <robertogomezlopez2005@gmail.com>
2024-05-23 00:00:19 +0200

    clase batallaDigital creada con variables, constructor y un metodo

--- src/batalla/BatallaDigital.java
+++ src/batalla/BatallaDigital.java
@@ -1,5 +1,22 @@
 package batalla;
 
-public class BatallaDigital {
+import java.util.Random;
+import java.util.Scanner;
 
+public class BatallaDigital {
+	
+	private Domador domador;
+    private Digimon enemigo;
+    Scanner leer = new Scanner(System.in);
+    
+	public BatallaDigital(Domador domador) {
+		this.domador = domador;
+        this.enemigo = generarDigimonAleatorio();
+	}
+	
+	private Digimon generarDigimonAleatorio() {
+        String[] digimones = {"Agumon", "Gabumon", "Patamon"};
+        String digimonElegido = digimones[new Random().nextInt(digimones.length)];
+        return new Digimon(digimonElegido);
+    }
 }
